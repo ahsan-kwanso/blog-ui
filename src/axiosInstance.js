f81a3060d097ed84363142c9d34ec4408d277ef2
ahsan-kwanso <m.ahsan@kwanso.com>
2024-08-06 16:16:01 +0500

    added token for all requests

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -4,4 +4,16 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
+//Now I don't have to include the token every time
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default axiosInstance;
