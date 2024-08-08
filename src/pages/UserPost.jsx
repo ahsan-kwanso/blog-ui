5584c85a58e86f320081bd324c6056c508103436
ahsan-kwanso <m.ahsan@kwanso.com>
2024-08-08 10:52:40 +0500

    added search button feature to userpost page

--- src/pages/UserPost.jsx
+++ src/pages/UserPost.jsx
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserPostList from "../components/Dashboard/UserPostList";
 import "./UserPost.css"; // Import your CSS file
 import { AuthContext } from "../context/AuthContext";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SearchPostList from "../components/Dashboard/SearchPostList";
 
 const UserPost = () => {
   const { signout } = useContext(AuthContext); // Use the context here
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
   const handleSignOut = () => {
     signout();
     navigate("/login"); // Redirect to signin page after signing out
@@ -24,6 +26,10 @@ const UserPost = () => {
 
   const handleProfile = () => {
     navigate("/profile");
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value); // Update search query on input change
   };
 
   return (
@@ -44,11 +50,22 @@ const UserPost = () => {
             type="text"
             placeholder="Search posts..."
             className="search-input"
+            value={searchQuery}
+            onChange={handleSearchChange} // Update search query on input change
           />
-          <FontAwesomeIcon icon={faUser} onClick={handleProfile} size="2x" />
+          <FontAwesomeIcon
+            icon={faUser}
+            onClick={handleProfile}
+            size="2x"
+            className="btn"
+          />
         </div>
       </header>
-      <UserPostList />
+      {searchQuery ? (
+        <SearchPostList searchQuery={searchQuery} /> // Render SearchPostList when there's a search query
+      ) : (
+        <UserPostList /> // Render PostList when there's no search query
+      )}
     </div>
   );
 };
